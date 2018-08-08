1edeb61a19d47a463ae1fcf5d2817baf7363da7b
Iampato <858wpwaweru@gmail.com>
2018-08-09 00:56:39 +0300

    Performance optimized using google web tool

--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,11 @@ from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
 from welcome.views import health
 
+
+sitemaps = {
+ 'posts': PostSitemap,
+}
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^health$', health),
@@ -13,10 +18,14 @@ urlpatterns = [
     url('blog/', include('blog.urls', namespace='blog', app_name='blog')),
     url('event/', include('Event.urls', namespace='Event', app_name='Event')),
     url('contact/', include('contact.urls', namespace='contact', app_name='contact')),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+		name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
     urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
+else:
+	urlpatterns += static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+	urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
